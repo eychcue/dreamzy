24cf7e7243ef2ec3e477f2951746b91248458173
Haroon Qahtan <hq.haroon1@gmail.com>
2023-09-02 09:18:17 -0400

    updated stripe version

--- lib/stripe.ts
+++ lib/stripe.ts
@@ -1,6 +1,7 @@
 import Stripe from "stripe"
 
 export const stripe = new Stripe(process.env.STRIPE_API_KEY!, {
-  apiVersion: "2022-11-15",
+  // apiVersion: "2022-11-15",
+  apiVersion: "2023-08-16",
   typescript: true,
 });
